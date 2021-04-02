1711073c8c8aabc71d35f0c5aa1cf9cf6ff3bd06
david <er.daviddabwali@gmail.com>
2021-04-02 12:11:59 +0530

    upto clear all

--- src/SingleTodo.jsx
+++ src/SingleTodo.jsx
@@ -1,45 +1,44 @@
 import axios from "axios"
 import React from "react"
 
-export default class SingleTodo extends React.Component {
+export default function SingleTodo(props) {
 
 
-    deleteData(deletedItem) {
+    function deleteData() {
 
-        //console.log(deletedItem, Deleteindex)
-        this.props.handler(deletedItem)
+        // console.log(props.id)
+        props.handler(props.id)
 
     }
 
-    onChangeCheck(select, event) {
-        // console.log(select, id, event.target.checked)
-        this.props.onchangeHandler(select, event)
+    function onChangeCheck(event) {
+        // console.log(props.data, event.target.checked)
+        props.onchangeHandler(props.data, event.target.checked)
     }
 
 
-    render() {
-        let linethrough = ""
-        if (this.props.data.complete) {
-            linethrough = "complete-item"
-        }
-
-        return (
-            <>
-                <h6
-                    className="list-group-item list-group-item-action list-group-item-secondary bg-secondry text-center">
-                    <h6 className={linethrough}> <input type="checkbox" class="form-check-input mt-2"
-                        onChange={this.onChangeCheck.bind(this, this.props.data)}
-                        checked={this.props.data.complete} />
-                        {this.props.title}
-                        <button type="button" class="btn btn-sm ml-1 " onClick={
-                            this.deleteData.bind(this, this.props.id)
-                        }>
-                            <img src="delete.png" height="15" width="15" />
-                        </button></h6> <br />
-
-                </h6>
-            </>
-
-        )
+    let linethrough = ""
+    if (props.data.complete) {
+        linethrough = "complete-item"
     }
+
+    return (
+        <>
+            <h6
+                className="list-group-item list-group-item-action list-group-item-secondary bg-secondry text-center">
+                <h6 className={linethrough}>
+                    <input type="checkbox" class="form-check-input mt-2"
+                        onChange={onChangeCheck}
+                        checked={props.data.complete}
+                    />
+                    {props.title}
+                    <button type="button" class="btn btn-sm ml-1 " onClick={deleteData}>
+                        <img src="delete.png" height="15" width="15" />
+                    </button></h6> <br />
+
+            </h6>
+        </>
+
+    )
+
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,136 +1,195 @@
 
-import React from "react"
+import React, { useState, useEffect } from "react"
 import SingleTodo from "./SingleTodo"
 import { BaseApi } from "./BaseUrl"
 import "./index.css";
-class TodoList extends React.Component {
+function TodoList() {
 
-  state = {
-    user: [
+  const [todoList, setTodoList] = useState([])
+  const [currentTitle, setcurrentTitle] = useState("")
 
-    ],
-    currentTitle: "",
-    complete: false
+  // state = {
+  //   user: [
 
-  }
+  //   ],
+  //   currentTitle: "",
+  //   complete: false
+
+  // }
 
 
 
-  componentDidMount() {
+  // componentDidMount() {
+  //   BaseApi.get("todos").then((res) => {
+  //     this.setState({
+  //       user: res.data
+  //     })
+  //   })
+
+  // }
+
+  // callback = (callbackData) => {
+  //   let indexData = null;
+  //   let newUser = [...this.state.user]
+  //   BaseApi.delete("todos/" + callbackData).then((res) => {
+  //     //console.log(res.data)
+  //   })
+  //   for (let i = 0; i < this.state.user.length; i++) {
+  //     if (this.state.user[i].id == callbackData) {
+  //       indexData = i;
+  //       // console.log(callbackData, indexData)
+  //       break;
+  //     }
+  //   }
+  //   if (indexData != null) {
+  //     newUser.splice(indexData, 1)
+  //     //console.log(newUser)
+  //     this.setState({
+  //       user: newUser
+  //     })
+  //   }
+
+
+  // }
+
+
+  // update(todo, callbackevent) {
+  //   let indexData = null;
+  //   let checked = callbackevent.target.checked;
+  //   let newUser = [...this.state.user]
+  //   console.log(todo.id)
+  //   BaseApi.patch("todos/" + todo.id, { complete: checked }).then((res) => {
+  //   })
+  //   for (let i = 0; i < this.state.user.length; i++) {
+  //     if (this.state.user[i].id == todo.id) {
+  //       indexData = i
+  //       break
+  //     }
+  //   }
+  //   newUser[indexData].complete = checked
+  //   this.setState({
+  //     user: newUser
+  //   })
+
+  // }
+
+
+
+
+  useEffect(function () {
     BaseApi.get("todos").then((res) => {
-      this.setState({
-        user: res.data
-      })
+      setTodoList([...res.data])
     })
+  }, []) // not add if we not use empty array??
 
-  }
+  function deleteTodo(item) {
 
-  callback = (callbackData) => {
-    let indexData = null;
-    let newUser = [...this.state.user]
-    BaseApi.delete("todos/" + callbackData).then((res) => {
-      //console.log(res.data)
-    })
-    for (let i = 0; i < this.state.user.length; i++) {
-      if (this.state.user[i].id == callbackData) {
-        indexData = i;
-        // console.log(callbackData, indexData)
-        break;
-      }
-    }
-    if (indexData != null) {
-      newUser.splice(indexData, 1)
-      //console.log(newUser)
-      this.setState({
-        user: newUser
-      })
-    }
-
-
-  }
-
-
-  update(todo, callbackevent) {
-    let indexData = null;
-    let checked = callbackevent.target.checked;
-    let newUser = [...this.state.user]
-    console.log(todo.id)
-    BaseApi.patch("todos/" + todo.id, { complete: checked }).then((res) => {
-    })
-    for (let i = 0; i < this.state.user.length; i++) {
-      if (this.state.user[i].id == todo.id) {
-        indexData = i
+    let index = null
+    let newTodo = [...todoList]
+    console.log(newTodo)
+    for (let i = 0; i < newTodo.length; i++) {
+      if (newTodo[i].id == item) {
+        index = i
         break
       }
     }
-    newUser[indexData].complete = checked
-    this.setState({
-      user: newUser
-    })
-
+    newTodo.splice(index, 1)
+    setTodoList(newTodo)
+    BaseApi.delete("todos/" + item).then((res) => { })
   }
 
+  function deleteAll() {
+    for (let i = 0; i < todoList.length; i++) {
+      BaseApi.delete("todos/" + todoList[i].id).then((res) => { })
+    }
+    // todoList.splice(0, todoList.length)
+    // todoList = []
+    setTodoList([])
+  }
 
-  getData() {
-    return this.state.user.map((singleTodo, i) => {
+  function update(todo, checked) {
+    // console.log(checked)
+    let index = null
+    let newTodo = [...todoList]
+    BaseApi.patch("todos/" + todo.id, { complete: checked }).then((res) => { })
+    for (let i = 0; i < newTodo.length; i++) {
+      if (newTodo[i].id == todo.id) {
+        index = i
+        break
+      }
+    }
+
+    newTodo[index].complete = checked
+    setTodoList([...newTodo])
+  }
+
+  function getData() {
+    return todoList.map((singleTodo, i) => {
       return <SingleTodo key={singleTodo.id} index={i} id={singleTodo.id} data={singleTodo}
-        title={singleTodo.title} handler={this.callback} onchangeHandler={this.update.bind(this)} />
+        title={singleTodo.title} handler={deleteTodo} onchangeHandler={update} />
     })
   }
-  changeData(event) {
-    //let newstate = { ...this.state }
-    //newstate.currentTitle = event.target.value
-    this.setState({
-      currentTitle: event.target.value
-    })
-  }
-  onSubmit = () => {
-    let newUser = [...this.state.user]
-    //console.log(newUser)
-    let newstate = {
-      title: this.state.currentTitle, // cozz state user has title property so for replace 
-      complete: false                //title in newstate we use title.property name should be same
-    }
-
-    if (this.state.currentTitle !== "") {
-      BaseApi.post("todos", newstate).then((res) => {
-        // console.log(res.data)
-        newUser.push(res.data)
-        this.setState({
-          user: newUser,
-          currentTitle: ""
-        })
-      })
-
-    }
-  }
-
-  render() {
-    let count;
-    if (this.state.user.length > 0) {
-      count = <p>You have {this.state.user.length} pending value</p>
-    }
-    return (
-      <div className="container mt-4">
-        <div className="row">
-          <div className="col"></div>
-          <div className="col jumbotron">
-            <h1>Todo App</h1>
-            <input placeholder="Add your new todo"
-              value={this.state.currentTitle}
-              onChange={this.changeData.bind(this)} style={{ width: "250px" }} />
-            <button type="button" class="btn btn-sm ml-3" onClick={this.onSubmit}>
-              <img src="plus.jpg" height="40" width="40" />
-            </button>
-            {count}
-            {this.getData()}
-            <button type="button" class="btn btn-sm ml-1 bg-primary" >Clear All</button>
-          </div>
-          <div className="col"></div>
-        </div>
-      </div>
+  function changeData(event) {
+    setcurrentTitle(
+      event.target.value
     )
   }
+  function onSubmit() {
+    let newTodo = [...todoList]
+    let singleTodo = {
+      title: currentTitle,
+      complete: false
+    }
+    if (currentTitle !== "") {
+      newTodo.push(singleTodo)
+      setTodoList(newTodo)
+      BaseApi.post("todos", singleTodo).then((res) => { })
+      setcurrentTitle("")
+    }
+  }
+
+  //   if (this.state.currentTitle !== "") {
+  //     BaseApi.post("todos", newstate).then((res) => {
+  //       console.log(res.data)
+  //       newUser.push(res.data)
+  //       this.setState({
+  //         user: newUser,
+  //         currentTitle: ""
+  //       })
+  //     })
+
+  //   }
+  // }
+
+
+  let count;
+  if (todoList.length > 0) {
+    count = <p>You have {todoList.length} pending value</p>
+  }
+  return (
+    <div className="container mt-4" >
+      <div className="row">
+        <div className="col"></div>
+        <div className="col jumbotron">
+          <h1>Todo App</h1>
+          <input placeholder="Add your new todo"
+            value={currentTitle}
+            onChange={changeData}
+            style={{ width: "250px" }} />
+          <button type="button" class="btn btn-sm ml-3"
+            onClick={onSubmit}
+          >
+            <img src="plus.jpg" height="40" width="40" />
+          </button>
+          {count}
+          {getData()}
+          <button type="button" class="btn btn-sm ml-1 bg-primary" onClick={deleteAll} >Clear All</button>
+        </div>
+        <div className="col"></div>
+      </div>
+    </div>
+  )
+
 }
 
 export default TodoList;
