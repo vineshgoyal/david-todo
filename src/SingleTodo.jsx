d0bb5bc9e0a1eaee761de20540ff1cfcaa6a1d5c
davidsunaria <80746656+davidsunaria@users.noreply.github.com>
2021-04-06 13:12:20 +0530

    added router (#3)
    * added router
    
    * without post and first day
    
    * post but not accurate
    
    * post succefully delete left
    
    * Delete1 (#5)
    
    * delete without for loop
    
    * delete by for loop
    
    * delete by for loop and without using index as a parameter
    
    * delete by for loop and without using index as a parameter
    
    Co-authored-by: david <er.daviddabwali@gmail.com>
    
    Co-authored-by: Vinesh Goyal <vineshgoyal1@gmail.com>

---
+++ src/SingleTodo.jsx
@@ -0,0 +1,33 @@
+import axios from "axios"
+import React from "react"
+
+export default class SingleTodo extends React.Component {
+
+
+    deleteData(deletedItem) {
+
+        //console.log(deletedItem, Deleteindex)
+        this.props.handler(deletedItem)
+
+    }
+
+
+    render() {
+
+        return (
+            <>
+                <h6
+                    className="list-group-item list-group-item-action list-group-item-secondary bg-secondry text-center">
+                    <input type="checkbox" class="form-check-input mt-2" value="" />
+                    {this.props.title}
+                    <button type="button" class="btn btn-sm ml-1 " onClick={
+                        this.deleteData.bind(this, this.props.id)
+                    }>
+                        <img src="delete.png" height="15" width="15" />
+                    </button>
+                </h6>
+            </>
+
+        )
+    }
+}
---
+++ src/TodoList.jsx
@@ -0,0 +1,111 @@
+
+import React from "react"
+import SingleTodo from "./SingleTodo"
+import { BaseApi } from "./BaseUrl"
+
+class TodoList extends React.Component {
+
+  state = {
+    user: [
+
+    ],
+    currentTitle: "",
+    complete: false
+
+  }
+
+
+
+  componentDidMount() {
+    BaseApi.get("todos").then((res) => {
+      this.setState({
+        user: res.data
+      })
+    })
+
+  }
+
+  callback = (callbackData) => {
+    let indexData = null;
+    let newUser = [...this.state.user]
+    BaseApi.delete("todos/" + callbackData).then((res) => {
+      //console.log(res.data)
+    })
+    for (let i = 0; i < this.state.user.length; i++) {
+      if (this.state.user[i].id == callbackData) {
+        indexData = i;
+        console.log(callbackData, indexData)
+        break;
+      }
+    }
+    newUser.splice(indexData, 1)
+    indexData = null
+    //console.log(newUser)
+    this.setState({
+      user: newUser
+    })
+
+  }
+
+
+  getData() {
+    return this.state.user.map((singleTodo, i) => {
+      return <SingleTodo key={singleTodo.id} index={i} id={singleTodo.id} title={singleTodo.title} handler={this.callback} />
+    })
+  }
+  changeData(event) {
+    //let newstate = { ...this.state }
+    //newstate.currentTitle = event.target.value
+    this.setState({
+      currentTitle: event.target.value
+    })
+  }
+  onSubmit = () => {
+    let newUser = [...this.state.user]
+    //console.log(newUser)
+    let newstate = {
+      title: this.state.currentTitle, // cozz state user has title property so for replace 
+      complete: false                //title in newstate we use title.property name should be same
+    }
+
+    if (this.state.currentTitle !== "") {
+      BaseApi.post("todos", newstate).then((res) => {
+        // console.log(res.data)
+        newUser.push(res.data)
+        this.setState({
+          user: newUser,
+          currentTitle: ""
+        })
+      })
+
+    }
+  }
+
+  render() {
+    let count;
+    if (this.state.user.length > 0) {
+      count = <p>You have {this.state.user.length} pending value</p>
+    }
+    return (
+      <div className="container mt-4">
+        <div className="row">
+          <div className="col"></div>
+          <div className="col jumbotron">
+            <h1>Todo App</h1>
+            <input placeholder="Add your new todo"
+              value={this.state.currentTitle}
+              onChange={this.changeData.bind(this)} style={{ width: "250px" }} />
+            <button type="button" class="btn btn-sm ml-3" onClick={this.onSubmit}>
+              <img src="plus.jpg" height="40" width="40" />
+            </button>
+            {count}
+            {this.getData()}
+          </div>
+          <div className="col"></div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default TodoList;
